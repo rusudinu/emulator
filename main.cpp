e95c023524ebda00b9d2dcf26ac3e9600a71330d
DDDECAR <dinustefan89.ds@gmail.com>
2020-07-30 16:41:16 +0300

    The Body Sprite Is Now Displayed

--- main.cpp
+++ main.cpp
@@ -5,14 +5,21 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 500), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(780, 332), "Atari 2600");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
     sf::RectangleShape rectShape;
     rectShape.setFillColor(sf::Color::Yellow);
-    rectShape.setPosition(200,200);
+    rectShape.setPosition(200,125);
     rectShape.setSize(sf::Vector2f(EMULATOR_WIDTH,EMULATOR_HEIGHT) *2.f);
+
+
+    sf::Texture bodyTexture;
+    if(!bodyTexture.loadFromFile("assets/BACKGROUND.PNG")){
+    // Handle an error.
+    }
+    sf::Sprite bodySprite(bodyTexture);
 
     while (window.isOpen())
     {
@@ -23,7 +30,9 @@ int main()
                 window.close();
         }
 
+
         window.clear();
+        window.draw(bodySprite);
         window.draw(rectShape);
         window.display();
     }
